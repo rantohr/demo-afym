54f4b13545d67a715d0ae11ba23a72e9221525cc
Ranto <ranto.andriamanalina@novity.io>
2025-11-16 14:11:20 +0300

    dashboard page

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,40 +2,37 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AgCharts } from 'ag-charts-angular';
 import { AgChartOptions } from 'ag-charts-community';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+import { FormControl, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, AgCharts],
+  imports: [CommonModule, AgCharts, FormsModule, MatFormFieldModule, MatSelectModule, MatDatepickerModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  providers: [provideNativeDateAdapter(), { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' }],
 })
 export class DashboardComponent {
   factureCharts: { title: string, chart: AgChartOptions }[] = [];
   tresorerieChart: AgChartOptions = {};
+  levels = [{ value: 'Admin', text: 'Admin' }, { value: 'Pro', text: 'Production' }, { value: 'Com', text: 'Commercial' },]
+  selectedLevel = 'Admin';
+  dateRange = { start: new Date('2025-01-01'), end: new Date('2025-12-31') };
 
   constructor() {
     this.factureCharts = [
       {
-        title: 'Mission Comptabilité', chart: {
+        title: 'Tenue Comptable & Fiscale', chart: {
           data: [
             { asset: "Facture réglée", amount: 60000 },
             { asset: "Facture non-réglée", amount: 40000 }
           ],
           series: [
-            { type: "pie", angleKey: "amount", legendItemKey: "asset" },
-          ],
-          theme: "ag-polychroma"
-        }
-      },
-      {
-        title: 'Mission Fiscalité', chart: {
-          data: [
-            { asset: "Facture réglée", amount: 23000 },
-            { asset: "Facture non-réglée", amount: 13000 }
-          ],
-          series: [
-            { type: "pie", angleKey: "amount", legendItemKey: "asset" },
+            { type: "donut", angleKey: "amount", legendItemKey: "asset" },
           ],
           theme: "ag-polychroma"
         }
@@ -47,7 +44,7 @@ export class DashboardComponent {
             { asset: "Facture non-réglée", amount: 40000 }
           ],
           series: [
-            { type: "pie", angleKey: "amount", legendItemKey: "asset" },
+            { type: "donut", angleKey: "amount", legendItemKey: "asset" },
           ],
           theme: "ag-polychroma"
         }
@@ -59,7 +56,7 @@ export class DashboardComponent {
             { asset: "Facture non-réglée", amount: 86000 }
           ],
           series: [
-            { type: "pie", angleKey: "amount", legendItemKey: "asset" },
+            { type: "donut", angleKey: "amount", legendItemKey: "asset" },
           ],
           theme: "ag-polychroma"
         }
@@ -71,7 +68,7 @@ export class DashboardComponent {
             { asset: "Facture non-réglée", amount: 11600 }
           ],
           series: [
-            { type: "pie", angleKey: "amount", legendItemKey: "asset" },
+            { type: "donut", angleKey: "amount", legendItemKey: "asset" },
           ],
           theme: "ag-polychroma"
         }
@@ -80,19 +77,19 @@ export class DashboardComponent {
 
     this.tresorerieChart = {
       data: [
-  { month: "Jan", comptabilite: 222, fiscalite: 400, juridique: 200, sociale: 244, exceptionnelle: 50 },
-  { month: "Fév", comptabilite: 210, fiscalite: 245, juridique: 195, sociale: 235, exceptionnelle: 55 },
-  { month: "Mar", comptabilite: 230, fiscalite: 265, juridique: 210, sociale: 250, exceptionnelle: 62 },
-  { month: "Avr", comptabilite: 240, fiscalite: 275, juridique: 205, sociale: 258, exceptionnelle: 66 },
-  { month: "Mai", comptabilite: 258, fiscalite: 290, juridique: 220, sociale: 268, exceptionnelle: 72 },
-  { month: "Juin", comptabilite: 270, fiscalite: 295, juridique: 218, sociale: 280, exceptionnelle: 76 },
-  { month: "Jul", comptabilite: 129, fiscalite: 305, juridique: 225, sociale: 290, exceptionnelle: 82 },
-  { month: "Août", comptabilite: 280, fiscalite: 320, juridique: 238, sociale: 300, exceptionnelle: 78 },
-  { month: "Sep", comptabilite: 292, fiscalite: 310, juridique: 245, sociale: 305, exceptionnelle: 88 },
-  { month: "Oct", comptabilite: 24, fiscalite: 335, juridique: 250, sociale: 310, exceptionnelle: 95 },
-  { month: "Nov", comptabilite: 320, fiscalite: 340, juridique: 260, sociale: 325, exceptionnelle: 102 },
-  { month: "Déc", comptabilite: 220, fiscalite: 350, juridique: 270, sociale: 335, exceptionnelle: 110 }
-],
+        { month: "Jan", comptabilite: 222, fiscalite: 400, juridique: 200, sociale: 244, exceptionnelle: 50 },
+        { month: "Fév", comptabilite: 210, fiscalite: 245, juridique: 195, sociale: 235, exceptionnelle: 55 },
+        { month: "Mar", comptabilite: 230, fiscalite: 265, juridique: 210, sociale: 250, exceptionnelle: 62 },
+        { month: "Avr", comptabilite: 240, fiscalite: 275, juridique: 205, sociale: 258, exceptionnelle: 66 },
+        { month: "Mai", comptabilite: 258, fiscalite: 290, juridique: 220, sociale: 268, exceptionnelle: 72 },
+        { month: "Juin", comptabilite: 270, fiscalite: 295, juridique: 218, sociale: 280, exceptionnelle: 76 },
+        { month: "Jul", comptabilite: 129, fiscalite: 305, juridique: 225, sociale: 290, exceptionnelle: 82 },
+        { month: "Août", comptabilite: 280, fiscalite: 320, juridique: 238, sociale: 300, exceptionnelle: 78 },
+        { month: "Sep", comptabilite: 292, fiscalite: 310, juridique: 245, sociale: 305, exceptionnelle: 88 },
+        { month: "Oct", comptabilite: 24, fiscalite: 335, juridique: 250, sociale: 310, exceptionnelle: 95 },
+        { month: "Nov", comptabilite: 320, fiscalite: 340, juridique: 260, sociale: 325, exceptionnelle: 102 },
+        { month: "Déc", comptabilite: 220, fiscalite: 350, juridique: 270, sociale: 335, exceptionnelle: 110 }
+      ],
 
       series: [
         { type: "line", xKey: "month", yKey: "comptabilite", yName: "Comptabilité" },
